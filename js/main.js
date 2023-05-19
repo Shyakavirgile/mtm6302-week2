e26ec7394a661a44f0bf2e1e7e780a7f47172e78
Shyaka Virgile Iradukunda <irad0010@algonquinlive.com>
2023-05-19 18:30:51 -0400

    Completes-class-demo

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// console.log("JS file included!")
+
+let myName = "Shyaka"
+console.log(myName)
+
+myName = "Shyaka"
+console.log(myName)
+
+const address = "Earth"
+console.log(address)
+
+let myProfession
+console.log(myProfession)
+
+myProfession = "Professor"
+console.log(myProfession)
+
+// Array
+const myScore = "[56, 76, 98]"
+console.log(myScore)
+
+let num = 10
+
+num +=2
+console.log(num)
+
+console.log(num === "12")
+
+console.log(!(num !== 90))
+
+console.log(num !== 90)
+
+console.log(num >= 7 && num !== 12 && myScore[0] < 98)
+
+console.log(num == 12)
+
+console.log(num == 12)
+
+const pets = 2;
+const pet1Name = "Simba"
+const pet2Name = "Bufi"
+
+console.log("I have " + pets + "dogs, " + pet1Name + "&" + pet2Name)
+console.log(`I have ${pets} dogs, ${pet1Name} & ${pet2Name}`)
